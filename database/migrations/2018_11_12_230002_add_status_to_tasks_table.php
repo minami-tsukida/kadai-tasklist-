6770b33f772cf69c8262510bf67155ea1724a915
minami-tsukida <heliotropium3@gmail.com>
2018-11-13 13:40:29 +0000

    add column

--- database/migrations/2018_11_12_230002_add_status_to_tasks_table.php
+++ database/migrations/2018_11_12_230002_add_status_to_tasks_table.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToTasksTable extends Migration
-{
+class AddStatusToTasksTable extends Migration
+{   
     /**
      * Run the migrations.
      *
@@ -14,7 +14,7 @@ class AddTitleToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->string('status',10);
         });
     }
 
@@ -26,7 +26,7 @@ class AddTitleToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 }
